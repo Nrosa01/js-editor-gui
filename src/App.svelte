408bea8952c229f4d2cb2a175a9095622689db3c
Rioni <nrosa01@ucm.es>
2022-12-09 19:27:54 +0100

    Si esto no va yo ya no sé

--- src/App.svelte
+++ src/App.svelte
@@ -10,10 +10,10 @@
     class="relative flex flex-col items-start align-center w-full h-screen bg-slate-600  overflow-hidden">
     <WindowsSystem />
     <AppBar>
-      <AppBarIcon src="/add.png" tooltip="Add Item" callback="{api.addItem}"/>
-      <AppBarIcon src="/crossc.png" tooltip="Clear Board" callback="{api.clear}"/>
-      <AppBarIcon src="/download.png" tooltip="Downlaod Board" callback="{api.saveToFile}"/>
-      <AppBarIcon src="/upload.png" tooltip="Load Board" callback="{api.loadFromFile}"/>
+      <AppBarIcon src="add.png" tooltip="Add Item" callback="{api.addItem}"/>
+      <AppBarIcon src="crossc.png" tooltip="Clear Board" callback="{api.clear}"/>
+      <AppBarIcon src="download.png" tooltip="Downlaod Board" callback="{api.saveToFile}"/>
+      <AppBarIcon src="upload.png" tooltip="Load Board" callback="{api.loadFromFile}"/>
     </AppBar>
   </div>
 </main>
