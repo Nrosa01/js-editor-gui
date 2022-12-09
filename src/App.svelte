036c8d05c6c8cd643160679b1e453801f7ee1f76
Rioni <nrosa01@ucm.es>
2022-12-09 20:03:53 +0100

    Icons imported from js

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import AppBarIcon from "./lib/AppBarIcon.svelte";
   import WindowsSystem from "./lib/WindowsSystem.svelte";
   import { api } from "./assets/api.js";
+  import icons from "./assets/icons.js";
 </script>
 
 <main>
@@ -10,10 +11,10 @@
     class="relative flex flex-col items-start align-center w-full h-screen bg-slate-600  overflow-hidden">
     <WindowsSystem />
     <AppBar>
-      <AppBarIcon src="add.png" tooltip="Add Item" callback="{api.addItem}"/>
-      <AppBarIcon src="crossc.png" tooltip="Clear Board" callback="{api.clear}"/>
-      <AppBarIcon src="download.png" tooltip="Downlaod Board" callback="{api.saveToFile}"/>
-      <AppBarIcon src="upload.png" tooltip="Load Board" callback="{api.loadFromFile}"/>
+      <AppBarIcon src="{icons.add}" tooltip="Add Item" callback="{api.addItem}"/>
+      <AppBarIcon src="{icons.crossc}" tooltip="Clear Board" callback="{api.clear}"/>
+      <AppBarIcon src="{icons.download}" tooltip="Downlaod Board" callback="{api.saveToFile}"/>
+      <AppBarIcon src="{icons.upload}" tooltip="Load Board" callback="{api.loadFromFile}"/>
     </AppBar>
   </div>
 </main>
---
+++ src/assets/icons.js
@@ -0,0 +1,11 @@
+import add from "/baricons/add.png";
+import crossc from "/baricons/crossc.png";
+import download from "/baricons/download.png";
+import upload from "/baricons/upload.png";
+
+export default {
+    add,
+    crossc,
+    download,
+    upload
+}
