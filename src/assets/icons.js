b346cbd1ce5efb5a7f5e21fce39ec4d0c3c1c2c0
Rioni <nrosa01@ucm.es>
2022-12-10 03:44:54 +0100

    Upsie

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,11 +1,7 @@
 import add from "/baricons/add.png";
-import crossc from "/baricons/crossc.png";
-import download from "/baricons/download.png";
 import upload from "/baricons/upload.png";
 
 export default {
     add,
-    crossc,
-    download,
     upload
 }
