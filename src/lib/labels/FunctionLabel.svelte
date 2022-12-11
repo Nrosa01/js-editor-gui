87333db8c3feb31d697e2ca05becf512253d66c8
Rioni <nrosa01@ucm.es>
2022-12-11 19:56:34 +0100

    Function field name processed

--- src/lib/labels/FunctionLabel.svelte
+++ src/lib/labels/FunctionLabel.svelte
@@ -1,11 +1,12 @@
 <script>
   import { api } from "../../assets/api.js";
+  import { processFieldName } from "../../assets/utils.js";
   export let fieldName = "Button";
   export let fieldValue;
   export let parent;
 
   function call() {
-    fieldValue.value(parent, api);
+    fieldValue.value(parent.value, api);
   }
 </script>
 
@@ -15,6 +16,6 @@
     type="button"
     class="w-full inline-block px-6 py-2 bg-slate-700 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-slate-800 hover:shadow-lg focus:bg-slate-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-000 active:shadow-lg transition duration-150 ease-in-out"
     on:click="{call}">
-    {fieldName}
+    {processFieldName(fieldName)}
   </button>
 </div>
